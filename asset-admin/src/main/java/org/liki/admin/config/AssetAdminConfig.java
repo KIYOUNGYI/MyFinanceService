b0676afe6d43331f376701aac6a9caf5fb62376b
KIYOUNGYI <yky2798@gmail.com>
2023-10-06 16:08:03 +0900

    10/6 (금) 삽질한 내용 반영

---
+++ asset-admin/src/main/java/org/liki/admin/config/AssetAdminConfig.java
@@ -0,0 +1,10 @@
+package org.liki.admin.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "org.liki.common")//common package 에 있는 bean 들을 scan 하기 위해 추가
+public class AssetAdminConfig {
+
+}
---
+++ asset-client/src/main/java/org/liki/client/adapter/in/web/HealthController.java
@@ -0,0 +1,21 @@
+package org.liki.client.adapter.in.web;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.liki.common.WebAdapter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@WebAdapter
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class HealthController {
+
+  @GetMapping("/api/health")
+  public String healthCheck() {
+    log.info("healthCheck() called");
+    return "Everything is fine!";
+  }
+
+}
